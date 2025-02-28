f905841f9d2f85d6d6d2683898a45917500a66c4
andrewwu531 <minghowu0531@gmail.com>
2025-02-28 01:37:59 +0000

    paypal checkout working completely - responsive design - desktop & laptop

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this path covers your components
+    // any other paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
