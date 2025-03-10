eeaee7687d9ab4fdf73c5b2ced8af044311c11e4
andrewwu531 <minghowu0531@gmail.com>
2025-03-10 22:30:42 +0000

    paypal works - new mobile frontend design v11 - desktop + laptop + mobile

--- src/components/HeaderMobilev2.jsx
+++ src/components/HeaderMobilev2.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import IconWhatsAppMobile from "./IconWhatsAppMobile";
-import IconTextMobile from "./IconTextMobile";
 import welcomeIcon from "../assets/welcome-icon.png"; // adjust the path as needed
 
 const BookingHeader = () => {
   return (
-    <header className="fixed top-0 left-0 z-50 w-full bg-indigo-700 ">
+    <header className="fixed top-0 left-0 z-50 w-full bg-white border-b border-b-gray-100 ">
       {/* Top Bar */}
-      <div className="flex items-center justify-start py-3 pl-5 mx-auto">
+      <div className="flex items-center justify-around py-3 pl-5 ">
         {/* Brand */}
         <div className="flex items-center">
           <img src={welcomeIcon} alt="Welcome Icon" className="w-10 h-10" />
         </div>
-        {/* <div className="px-6 py-3 font-semibold text-white bg-indigo-600 rounded-xl">
+        <div className="px-6 py-3 mx-3 font-semibold text-white bg-indigo-600 rounded-xl">
           Contact 07943 059792
-        </div> */}
+        </div>
       </div>
     </header>
   );
