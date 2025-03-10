93fa3847e7989d651838bee961d690db1c90360b
andrewwu531 <minghowu0531@gmail.com>
2025-03-10 22:42:13 +0000

    paypal works - new mobile frontend design v12 - desktop + laptop + mobile

--- src/components/PaymentLayoutMobile.jsx
+++ src/components/PaymentLayoutMobile.jsx
@@ -5,8 +5,8 @@ const PaymentLayoutMobile = ({ children }) => (
     id="booking-form"
     className="scroll-mt-[160px] w-5/6 mx-auto mt-10 mb-15"
   >
-    <div className="relative flex items-center justify-center mx-auto mb-1 text-white bg-indigo-600 rounded-lg py-7">
-      <h1 className="text-xl font-semibold">Book Appointment</h1>
+    <div className="relative flex items-center justify-start mx-auto mb-1 text-white bg-indigo-600 rounded-lg py-7">
+      <h1 className="pl-6 text-xl font-semibold">Book Appointment</h1>
     </div>
     <div className="grid grid-cols">
       <div className="flex justify-center col-span-2 pt-8">{children}</div>
