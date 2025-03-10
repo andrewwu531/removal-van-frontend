b0fbfc7d6f4bef882f8999458372b1922d78dd72
andrewwu531 <minghowu0531@gmail.com>
2025-03-10 00:54:39 +0000

    paypal works - completed all for first mobile screen size frontend - desktop + laptop + mobile

--- src/components/PriceCalculator.jsx
+++ src/components/PriceCalculator.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import PriceCalculatorDesktop from "./PriceCalculatorDesktop";
+import PriceCalculatorDesktop from "./PriceCalculatorDesktop1";
 import PriceCalculatorMobile from "./PriceCalculatorMobile";
 
 const PriceCalculator = () => {
