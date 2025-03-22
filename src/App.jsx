09e6c01dd349e8eaa178b7a3cb1fa1342d246d81
andrewwu531 <minghowu0531@gmail.com>
2025-03-22 07:38:50 +0000

    frontend & backend - LC responsive trade five photos - not M

--- src/App.jsx
+++ src/App.jsx
@@ -184,6 +184,8 @@ function App() {
                   <TraderDetailsDesktop />
                 </div>
                 <FooterDesktop />
+
+                <FooterDesktop />
               </>
             }
           />
