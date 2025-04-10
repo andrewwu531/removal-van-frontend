e6a362a7dd39aa16ad27704c4035288539df82f1
andrewwu531 <minghowu0531@gmail.com>
2025-04-11 00:39:38 +0100

    (reusable - paypal + email confirmation + population scripts + /trader-id refresh + headerSearch) cookies width

--- src/components/layout/cookie/CookieBanner.jsx
+++ src/components/layout/cookie/CookieBanner.jsx
@@ -7,7 +7,7 @@ export default function CookieBanner() {
   if (!showBanner) return null;
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 shadow-lg">
+    <div className="fixed z-50 left-1/2 transform -translate-x-1/2 bottom-3 w-[calc(100%-10%)] min-[1200px]:w-[calc(100%-20%)]  min-[1370px]:w-[calc(100%-30%)]  bg-white border-t border-gray-200 shadow-lg rounded-3xl">
       <CookieContent onAccept={handleAccept} />
     </div>
   );
--- src/components/layout/cookie/constants/cookieOptions.js
+++ src/components/layout/cookie/constants/cookieOptions.js
@@ -1,5 +1,5 @@
 export const cookieOptions = {
   bannerText:
-    "We use cookies to enhance your experience. By continuing to visit this site, you agree to our use of cookies.",
+    "We use cookies to enhance your experience. By continuing to visit the site, you agree to our use of cookies.",
   acceptButtonText: "Accept",
 };
