be0d49d53bcbe7b489d386409eef3405b452941e
andrewwu531 <minghowu0531@gmail.com>
2025-05-22 00:01:05 +0100

    removal pricing

--- src/components/forms/booking/Stage3Confirmation.jsx
+++ src/components/forms/booking/Stage3Confirmation.jsx
@@ -55,9 +55,6 @@ const Stage3Confirmation = ({ trader, formData, transactionId }) => {
             year: "numeric",
             month: "2-digit",
             day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
           }
         );
 
@@ -158,9 +155,6 @@ const Stage3Confirmation = ({ trader, formData, transactionId }) => {
             year: "numeric",
             month: "2-digit",
             day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
           })}
         </div>
         <div>
